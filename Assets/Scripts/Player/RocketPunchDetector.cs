9d7daa557b4f89834198a6f60dd8540508fc239f
DESKTOP-OST155Q\USER <numachinumachi0622@gmail.com>
2025-05-25 15:03:22 +0900

    add rocket-punch-system

---
+++ Assets/Scripts/Player/RocketPunchDetector.cs
@@ -0,0 +1,23 @@
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class RocketPunchDetector : MonoBehaviour
+{
+    private int _detectCount = 0;
+    public void Start()
+    {
+        var layerMask = LayerMask.GetMask("Detector");
+        var colliders = new Collider[2];
+        
+        this.UpdateAsObservable()
+            .Select(count => _detectCount = Physics.OverlapSphereNonAlloc(
+                transform.position, 0.5f, colliders, layerMask))
+            .Subscribe(count =>
+            {
+                _detectCount = count;
+                Debug.Log(_detectCount);
+            })
+            .AddTo(this);
+    }
+}
