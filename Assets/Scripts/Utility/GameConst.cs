18485d180a66a1b2c43d6aebaea328f6e7394721
Numachi0622 <numachinumachi0622@gmail.com>
2025-05-27 17:01:14 +0900

    add boss-assets

--- Assets/Scripts/Utility/GameConst.cs
+++ Assets/Scripts/Utility/GameConst.cs
@@ -8,7 +8,7 @@ public static class GameConst
     public const int ANIMATION_TARGET_FPS = 30;
     public const float DEMA_FILTER_ALPHA = 0.1f;
     public const float MAX_PUNCH_POINT = 100f;
-    public const float MAX_ARM_SCALE = 4f;
+    public const float MAX_ARM_SCALE = 3f;
     public const float MAX_PUNCH_VELOCITY = 20f;
     public const float PUNCH_POINT_RESET_TIME = 2f;
 }
