6521b136535ee99c5f3b0c82047f092a48b96e72
Numachi0622 <numachinumachi0622@gmail.com>
2025-11-05 04:32:59 +0900

    Extensionsを追加

---
+++ Assets/Scripts/Utility/Extensions/QuaternionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utility.Extensions
+{
+    public static class QuaternionExtensions
+    {
+        public static Quaternion ToQuaternion(this Windows.Kinect.Vector4 vector, Quaternion comp)
+        {
+            return Quaternion.Inverse(comp) * new Quaternion(-vector.X, -vector.Y, vector.Z, vector.W);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Extensions/VectorExtensions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Utility.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 ToVector3(this Windows.Kinect.Vector4 vector)
+        {
+            return new Vector3(vector.X, vector.Y, vector.Z);
+        }
+        
+        public static Windows.Kinect.Vector4 ToMirror(this Windows.Kinect.Vector4 vector)
+        {
+            return new Windows.Kinect.Vector4()
+            {
+                X = vector.X,
+                Y = -vector.Y,
+                Z = -vector.Z,
+                W = vector.W
+            };
+        }
+    }
+}
